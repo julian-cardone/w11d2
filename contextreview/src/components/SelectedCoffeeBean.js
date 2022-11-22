1818f7989e1cc6e4e9e5aca658965668c106a84a
kkj2010 <kunjukim0210@gmail.com>
2022-11-22 15:04:01 -0500

    done for contextreveiw

--- contextreview/src/components/SelectedCoffeeBean.js
+++ contextreview/src/components/SelectedCoffeeBean.js
@@ -1,5 +1,14 @@
+import { CoffeeContext } from "../context/CoffeeContext";
+import { useContext } from "react";
+
+
 const SelectedCoffeeBean = () => {
-  return null;
+  const {coffeeBean} = useContext(CoffeeContext); // to use context we need to use useContext()
+  return (
+
+  <div className="selected-coffee">
+    <h2>{coffeeBean.name}</h2>
+  </div>);
 }
 
 export default SelectedCoffeeBean;
